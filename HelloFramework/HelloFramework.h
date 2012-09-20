1a4e07f3cab37a8f0360c7bfb17070c145d052bb
Chad Gibbons <dcgibbons@gmail.com>
2012-09-20 08:26:27 -0600

    Initial Commit

---
+++ HelloFramework/HelloFramework.h
@@ -0,0 +1,13 @@
+//
+//  HelloFramework.h
+//  HelloFramework
+//
+//  Created by Chad Gibbons on 9/20/12.
+//  Copyright (c) 2012 Nuclear Bunny Studios, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HelloFramework : NSObject
+
+@end
---
+++ HelloFramework/HelloFramework.m
@@ -0,0 +1,13 @@
+//
+//  HelloFramework.m
+//  HelloFramework
+//
+//  Created by Chad Gibbons on 9/20/12.
+//  Copyright (c) 2012 Nuclear Bunny Studios, LLC. All rights reserved.
+//
+
+#import "HelloFramework.h"
+
+@implementation HelloFramework
+
+@end
